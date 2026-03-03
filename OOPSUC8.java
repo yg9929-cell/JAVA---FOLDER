bcc432c2be81a0fc9612e2496fdd7f25a533eb71
YOSHITHA GATTATH <yg9929@srmist.edu.in>
2026-03-03 11:15:25 +0530

    uc2

---
+++ OOPSUC8.java
@@ -0,0 +1,62 @@
+public class OOPSUC8 {
+    import java.util.HashMap;
+
+public static HashMap<Character, String[]> createCharacterMap() {
+
+    HashMap<Character, String[]> map = new HashMap<>();
+
+    map.put('O', new String[]{
+            " *****  ",
+            "*     * ",
+            "*     * ",
+            "*     * ",
+            "*     * ",
+            "*     * ",
+            " *****  "
+    });
+
+    map.put('P', new String[]{
+            " *****  ",
+            "*     * ",
+            "*     * ",
+            " *****  ",
+            "*       ",
+            "*       ",
+            "*       "
+    });
+
+    map.put('S', new String[]{
+            " ****** ",
+            "*       ",
+            "*       ",
+            " *****  ",
+            "      * ",
+            "      * ",
+            " ****** "
+    });
+
+    return map;
+}
+
+public static void displayBanner(String message, HashMap<Character, String[]> map) {
+
+    for (int i = 0; i < 7; i++) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (char ch : message.toCharArray()) {
+            String[] pattern = map.get(ch);
+            builder.append(pattern[i]).append("  ");
+        }
+
+        System.out.println(builder.toString());
+    }
+}
+
+public static void main(String[] args) {
+
+    HashMap<Character, String[]> map = createCharacterMap();
+    String message = "OOPS";
+    displayBanner(message, map);
+}
+
---
+++ OOPSstep.java
@@ -0,0 +1,14 @@
+public class OOPSstep {
+    public class OOPS {
+    public static void main(String[] args) {
+        System.out.println("   ***     " + "   ***     " + " ******    " + "     ***** ");
+        System.out.println(" **   **   " + " **   **   " + "**    **   " + "  **       ");
+        System.out.println("**     **  " + "**     **  " + "**     **  " + "**         ");
+        System.out.println("**     **  " + "**     **  " + "**     **  " + "  *****    ");
+        System.out.println("**     **  " + "**     **  " + " ******    " + "      **   ");
+        System.out.println("**     **  " + "**     **  " + "**         " + "        ** ");
+        System.out.println("**     **  " + "**     **  " + "**         " + "       **  ");
+        System.out.println(" **   **   " + " **   **   " + "**         " + "       **  ");
+        System.out.println("   ***     " + "   ***     " + "**         " + "  *****    ");
+    }
+}
